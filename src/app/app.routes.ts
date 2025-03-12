7c29a50ed9f313066cea3fe0e55e35c41fd619ed
Bodaa004 <abdallah.arafaa.2004@gmail.com>
2025-03-12 14:07:23 +0200

    Edit routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,69 +10,97 @@ export const routes: Routes = [
   {
     path: '',
     component: AuthLayoutComponent,
-    canActivate:[logedGuard],
+    canActivate: [logedGuard],
     children: [
-      { 
-        path: 'login', 
-        loadComponent: () => import('./pages/login/login.component').then(m => m.LoginComponent),
-        title: 'login'
+      {
+        path: 'login',
+        loadComponent: () =>
+          import('./pages/login/login.component').then((m) => m.LoginComponent),
+        title: 'login',
       },
-      { 
-        path: 'register', 
-        loadComponent: () => import('./pages/register/register.component').then(m => m.RegisterComponent),
-        title: 'register' 
+      {
+        path: 'register',
+        loadComponent: () =>
+          import('./pages/register/register.component').then(
+            (m) => m.RegisterComponent
+          ),
+        title: 'register',
       },
-      { 
-        path: 'forgot', 
-        loadComponent: () => import('./pages/forgot-password/forgot-password.component').then(m => m.ForgotPasswordComponent),
-        title: 'forgot Password' 
+      {
+        path: 'forgot',
+        loadComponent: () =>
+          import('./pages/forgot-password/forgot-password.component').then(
+            (m) => m.ForgotPasswordComponent
+          ),
+        title: 'forgot Password',
       },
     ],
   },
   {
     path: '',
     component: BlankLayoutComponent,
-    canActivate:[authGuard],
+    canActivate: [authGuard],
     children: [
-      { 
-        path: 'home', 
-        loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent),
-        title: 'home'
+      {
+        path: 'home',
+        loadComponent: () =>
+          import('./pages/home/home.component').then((m) => m.HomeComponent),
+        title: 'home',
       },
-      { 
-        path: 'cart', 
-        loadComponent: () => import('./pages/car/car.component').then(m => m.CarComponent),
-        title: 'cart'
+      {
+        path: 'cart',
+        loadComponent: () =>
+          import('./pages/car/car.component').then((m) => m.CarComponent),
+        title: 'cart',
       },
-      { 
-        path: 'products', 
-        loadComponent: () => import('./pages/products/products.component').then(m => m.ProductsComponent),
-        title: 'products'
+      {
+        path: 'products',
+        loadComponent: () =>
+          import('./pages/products/products.component').then(
+            (m) => m.ProductsComponent
+          ),
+        title: 'products',
       },
-      { 
-        path: 'brands', 
-        loadComponent: () => import('./pages/brands/brands.component').then(m => m.BrandsComponent),
-        title: 'brands'
+      {
+        path: 'brands',
+        loadComponent: () =>
+          import('./pages/brands/brands.component').then(
+            (m) => m.BrandsComponent
+          ),
+        title: 'brands',
       },
-      { 
-        path: 'categories', 
-        loadComponent: () => import('./pages/categories/categories.component').then(m => m.CategoriesComponent),
-        title: 'categories'
+      {
+        path: 'categories',
+        loadComponent: () =>
+          import('./pages/categories/categories.component').then(
+            (m) => m.CategoriesComponent
+          ),
+        title: 'categories',
       },
-      { 
-        path: 'checkout/:id', 
-        loadComponent: () => import('./pages/checkout/checkout.component').then(m => m.CheckoutComponent),
-        title: 'checkout'
+      {
+        path: 'checkout/:id',
+        loadComponent: () =>
+          import('./pages/checkout/checkout.component').then(
+            (m) => m.CheckoutComponent
+          ),
+        title: 'checkout',
+        data: { renderMode: 'no-prerender' }
       },
-      { 
-        path: 'details/:id', 
-        loadComponent: () => import('./pages/details/details.component').then(m => m.DetailsComponent),
-        title: 'details'
+      {
+        path: 'details/:id',
+        loadComponent: () =>
+          import('./pages/details/details.component').then(
+            (m) => m.DetailsComponent
+          ),
+        title: 'details',
       },
-      { 
-        path: 'allorders', 
-        loadComponent: () => import('./pages/allorders/allorders.component').then(m => m.AllordersComponent),
-        title: 'All Orders'
+      {
+        path: 'allorders',
+        loadComponent: () =>
+          import('./pages/allorders/allorders.component').then(
+            (m) => m.AllordersComponent
+          ),
+        title: 'All Orders',
       },
       { path: '**', component: NotfoundComponent },
     ],
